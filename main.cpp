508523bf9f28f34096cc2cb44d66bf756e048783
Алина Пахомова <alinapahomova@MacBook-Air-Alina.local>
2023-08-15 18:15:40 +0300

    Описание структуры заголовка файла, описание таблицы паспортов параметров, описание поля форматов, получения числа параметров, получение имя параметра

--- main.cpp
+++ main.cpp
@@ -2,6 +2,42 @@
 #include <iostream>
 #include <sstream>
 
+uint16_t readHeader(char *, size_t);
+
+struct USMLHeader
+{
+    uint32_t marker;
+    char nameProduct[8];
+    char nameTest[8];
+    char date[8];
+    uint16_t countParamrters;
+    uint16_t reserve;
+};
+
+struct USMLPasportTable
+{
+    char nameParametrs[12];
+    char nameProperties[12];
+    uint64_t dimension;
+    uint32_t discreteness;
+    float K0;
+    float K1;
+    uint16_t lengthArray;
+    uint8_t format;
+    float Tn;
+    float Tk;
+    uint8_t reserve;
+};
+
+enum USMLFormatData
+{
+    dataChar = 1,
+    dataShort = 2,
+    dataLong = 3,
+    dataFloat = 4,
+    dataDouble = 8
+};
+
 int main(int argc, const char **argv)
 {
 
@@ -29,19 +65,83 @@ int main(int argc, const char **argv)
 
     char *fileBuffer = new char[fileSize];
 
+    const int fileMarker = 2341245331;
 
+    file.read(fileBuffer, fileSize);
+    file.close();
+    uint16_t parametrCount = readHeader(fileBuffer, fileSize);
 
-
-
-
-
-
-
-
-
-
+    char *startTableParametrs = &fileBuffer[32]; // Начало таблицы паспортов
 
     delete[] fileBuffer;
-    file.close();
+
     return 0;
+}
+
+uint16_t readHeader(char *buffer, size_t size)
+{
+    const int fileMarker = 2341245331;
+
+    USMLHeader *usmlheader = (USMLHeader *)buffer; // Преобразование типа
+    if (usmlheader->marker != fileMarker)
+    {
+        std::cerr << "Marker not found" << std::endl;
+        return -1;
+    }
+    return usmlheader->countParamrters;
+}
+
+std::string &GetParametrName(char *buffer)
+{
+    USMLPasportTable *usmlpasportable = (USMLPasportTable *)buffer;
+    std::string outputName = usmlpasportable->nameParametrs;
+    return outputName;
+}
+
+void GetParametrData(std::fstream &parametrFile, USMLPasportTable &parametrPropeties, char *buffer)
+{
+    if (parametrFile.is_open() == false)
+    {
+        std::cerr << "Error create file" << std::endl;
+        return;
+    }
+
+    int data = 0;
+
+    switch (parametrPropeties.format)
+    {
+    case dataChar:
+        for (size_t i = 0; i < parametrPropeties.lengthArray; ++i)
+        {
+            data = buffer[i];
+        }
+        break;
+
+    case dataShort:
+            for (size_t i = 0; i < parametrPropeties.lengthArray; ++i)
+        {
+            data = buffer[i];
+        }
+        break;
+    case dataLong:
+            for (size_t i = 0; i < parametrPropeties.lengthArray; ++i)
+        {
+            data = buffer[i];
+        }
+        break;
+    case dataFloat:
+            for (size_t i = 0; i < parametrPropeties.lengthArray; ++i)
+        {
+            data = buffer[i];
+        }
+        break;
+    case dataDouble:
+            for (size_t i = 0; i < parametrPropeties.lengthArray; ++i)
+        {
+            data = buffer[i];
+        }
+        break;
+    default:
+        break;
+    };
 }
